94d2cc862f03c358c25006804c8d0bc61c041234
igrumi <ignacio.grumi@gmail.com>
2026-03-01 18:22:30 -0300

    add: file for emotes/emote setup function

---
+++ src/utils/emotes.py
@@ -0,0 +1,34 @@
+import discord
+
+kase = kasen = angii = angi = bleh = blehh = blehhhh = None
+mrballs = mrpenis = mrtitties = pn = pp = happy = mm = None
+tomatewn = hm = oo = trite = jojojo = oshahappy = okk = tite = None
+
+def setup_emotes(bot):
+    """Esta función asignará los emojis a las variables globales"""
+    global kase, kasen, angii, angi, bleh, blehh, blehhhh
+    global mrballs, mrpenis, mrtitties, pn, pp, happy, mm
+    global tomatewn, oo, trite, jojojo, oshahappy, okk, tite, hm
+
+    kase = discord.utils.get(bot.emojis, name="kase")
+    kasen = discord.utils.get(bot.emojis, name="kasen")
+    angii = discord.utils.get(bot.emojis, name="angii")
+    angi = discord.utils.get(bot.emojis, name="angi")
+    bleh = discord.utils.get(bot.emojis, name="bleh")
+    blehh = discord.utils.get(bot.emojis, name="blehh")
+    blehhhh = discord.utils.get(bot.emojis, name="blehhhh")
+    mrballs = discord.utils.get(bot.emojis, name="mrballs")
+    mrpenis = discord.utils.get(bot.emojis, name="mrpenis")
+    mrtitties = discord.utils.get(bot.emojis, name="mrtitties")
+    pn = discord.utils.get(bot.emojis, name="pn")
+    pp = discord.utils.get(bot.emojis, name="pp")
+    happy = discord.utils.get(bot.emojis, name="happy")
+    mm = discord.utils.get(bot.emojis, name="mm")
+    tomatewn = discord.utils.get(bot.emojis, name="tomatewn")
+    oo = discord.utils.get(bot.emojis, name="oo")
+    trite = discord.utils.get(bot.emojis, name="trite")
+    jojojo = discord.utils.get(bot.emojis, name="jojojo")
+    oshahappy = discord.utils.get(bot.emojis, name="emoji_52")
+    okk = discord.utils.get(bot.emojis, name="okk")
+    tite = discord.utils.get(bot.emojis, name="tite")
+    hm = discord.utils.get(bot.emojis, name="hm")
